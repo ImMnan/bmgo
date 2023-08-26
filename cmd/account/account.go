7cdfa998422ff0d81e0dfd8085595e57e2f88f59
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-08-26 14:39:51 +0530

    Adding Subcommands

---
+++ cmd/account/account.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package account
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// accountCmd represents the account command
+var AccountCmd = &cobra.Command{
+	Use:   "account",
+	Short: "This will get info about the account",
+	Long:  `NOt much to say at least`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("account called")
+		accountId, _ := cmd.Flags().GetInt("id")
+		getAccountId(accountId)
+
+	},
+}
+
+func init() {
+	AccountCmd.PersistentFlags().Int("id", 0, "confirm the account id")
+	AccountCmd.MarkPersistentFlagRequired("id")
+}
+
+func getAccountId(accountId int) {
+	fmt.Println("this is the account ID passed:", accountId)
+}
---
+++ cmd/account/invitation.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package account
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// invitationCmd represents the invitation command
+var invitationCmd = &cobra.Command{
+	Use:   "invitation",
+	Short: "A brief description of your command",
+	Long:  `A longer description that spans multiple lines and likely`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("invitation called")
+		accountId, _ := cmd.Flags().GetInt("id")
+		invitations(accountId)
+	},
+}
+
+func init() {
+	AccountCmd.AddCommand(invitationCmd)
+}
+
+func invitations(accountId int) {
+	vp := viper.New()
+	vp.SetConfigName("api-key")
+	vp.SetConfigType("json")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiId := vp.GetString("id")
+	apiSecret := vp.GetString("secret")
+
+	client := &http.Client{}
+	accountIdStr := strconv.Itoa(accountId)
+	fmt.Println("Account scanned is: ", accountIdStr)
+	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/invitations", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth(apiId, apiSecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
+}
