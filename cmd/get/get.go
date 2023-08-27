0a97c1a610ae830d2eec1997b0cdcc81c21f3180
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-08-27 15:00:10 +0530

    Created main root cmd

---
+++ cmd/get/get.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package get
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// getCmd represents the get command
+var GetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Use get command for listing the information",
+	Long:  `This is a get subcommad for listing info, related to account info, workspaces, users, invitation etc.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func Getapikeys() (string, string) {
+	vp := viper.New()
+	vp.SetConfigName("api-key")
+	vp.SetConfigType("json")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiId := vp.GetString("id")
+	apiSecret := vp.GetString("secret")
+	return apiId, apiSecret
+}
