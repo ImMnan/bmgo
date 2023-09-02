79a3a550ef1cc7c7d06811a22980ce48394e580e
Manan <immnan333@gmail.com>
2023-09-02 11:53:51 +0530

    New subcommand

---
+++ cmd/find/find.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package find
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// findCmd represents the find command
+var FindCmd = &cobra.Command{
+	Use:   "find",
+	Short: "Used to find",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func Getapikeys() (string, string) {
+	vp := viper.New()
+	vp.SetConfigName("api-key")
+	vp.SetConfigType("json")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiId := vp.GetString("id")
+	apiSecret := vp.GetString("secret")
+	return apiId, apiSecret
+}
