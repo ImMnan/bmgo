8e2da906ffff2228f97c55a64a97311aece8fbbe
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-09-17 20:00:12 +0530

    Completed update user cmd, conditions added, formatted output

--- cmd/update/user.go
+++ cmd/update/user.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package update
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -21,7 +22,23 @@ var userCmd = &cobra.Command{
 	Short: "Update users in Account or Workspace",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("user called")
+		userId, _ := cmd.Flags().GetInt("uid")
+		accountId, _ := cmd.Flags().GetInt("accountid")
+		workspaceId, _ := cmd.Flags().GetInt("workspaceid")
+		rawOutput, _ := cmd.Flags().GetBool("raw")
+		switch {
+		case (workspaceId != 0) && (accountId == 0) && rawOutput:
+			updateUserWsraw(userId, workspaceId)
+		case (workspaceId == 0) && (accountId != 0) && rawOutput:
+			updateUserAraw(userId, accountId)
+		case (workspaceId != 0) && (accountId == 0) && !rawOutput:
+			updateUserWs(userId, workspaceId)
+		case (workspaceId == 0) && (accountId != 0) && !rawOutput:
+			updateUserA(userId, accountId)
+		default:
+			fmt.Println("\nPlease provide a correct workspace Id or Account Id to get the info")
+			fmt.Println("[bmgo update -a <account_id>...] OR [bmgo update -w <workspace_id>...]")
+		}
 	},
 }
 
@@ -31,32 +48,170 @@ func init() {
 	userCmd.MarkFlagRequired("uid")
 }
 
-func userRoleSelectorA() string {
+func updateUserSelectorA() (string, bool) {
 	prompt := promptui.Select{
-		Label: "Select Role",
+		Label: "Select Account Role:",
 		Items: []string{"admin", "standard", "user_manager", "billing"},
 	}
+	prompt1 := promptui.Select{
+		Label: "Enable:",
+		Items: []bool{true, false},
+	}
 	_, roleSelected, err := prompt.Run()
-
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 	}
-	fmt.Printf("You choose %q\n", roleSelected)
-	return roleSelected
+	_, attachAuto, err := prompt1.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+	}
+	boolVal, _ := strconv.ParseBool(attachAuto)
+	return roleSelected, boolVal
+}
+func updateUserSelectorWs() (string, bool) {
+	prompt := promptui.Select{
+		Label: "Select Account Role:",
+		Items: []string{"tester", "manager", "viewer"},
+	}
+	prompt1 := promptui.Select{
+		Label: "Enable:",
+		Items: []bool{true, false},
+	}
+	_, roleSelected, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+	}
+	_, attachAuto, err := prompt1.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+	}
+	boolVal, _ := strconv.ParseBool(attachAuto)
+	return roleSelected, boolVal
 }
 
-func updateUserByUidA(userId, accountId int) {
-	roleA := userRoleSelectorA()
+type updateUserResponse struct {
+	Result updateUserResult `json:"result"`
+}
+type updateUserResult struct {
+	Email   string   `json:"email"`
+	Enabled bool     `json:"enabled"`
+	Type    string   `json:"type"`
+	Roles   []string `json:"roles"`
+}
+
+func updateUserA(userId, accountId int) {
+	roleA, enableA := updateUserSelectorA()
 	apiId, apiSecret := Getapikeys()
 	accountIdStr := strconv.Itoa(accountId)
 	client := &http.Client{}
 	userIdStr := strconv.Itoa(userId)
 	// var data = strings.NewReader(`{"roles":["user_manager"],"enabled": false}`)
-	data := fmt.Sprintf(`{"roles": ["%s"], "enabled": true}`, roleA)
+	data := fmt.Sprintf(`{"roles": ["%s"], "enabled": %t}`, roleA, enableA)
 	fmt.Println(data)
-	var reqBodyData = strings.NewReader(data)
-	fmt.Println(reqBodyData)
-	req, err := http.NewRequest("PUT", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users/"+userIdStr, reqBodyData)
+	var reqBodyDataA = strings.NewReader(data)
+	req, err := http.NewRequest("PUT", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users/"+userIdStr, reqBodyDataA)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(apiId, apiSecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("%s\n", bodyText)
+	var responseBodyUpdateUserA updateUserResponse
+	var userRoleA string
+	json.Unmarshal(bodyText, &responseBodyUpdateUserA)
+	userEmailA := responseBodyUpdateUserA.Result.Email
+	userTypeA := responseBodyUpdateUserA.Result.Type
+	userStatusA := responseBodyUpdateUserA.Result.Enabled
+	fmt.Printf("\n%-25s %-12s %-10s %-10s", "EMAIL", "TYPE", "ENABLE", "ROLE")
+	for i := 0; i < len(responseBodyUpdateUserA.Result.Roles); i++ {
+		userRoleA = responseBodyUpdateUserA.Result.Roles[i]
+	}
+	fmt.Printf("\n%-25s %-12s %-10t %-10s\n\n", userEmailA, userTypeA, userStatusA, userRoleA)
+}
+func updateUserAraw(userId, accountId int) {
+	roleA, enableA := updateUserSelectorA()
+	apiId, apiSecret := Getapikeys()
+	accountIdStr := strconv.Itoa(accountId)
+	client := &http.Client{}
+	userIdStr := strconv.Itoa(userId)
+	// var data = strings.NewReader(`{"roles":["user_manager"],"enabled": false}`)
+	data := fmt.Sprintf(`{"roles": ["%s"], "enabled": %t}`, roleA, enableA)
+	fmt.Println(data)
+	var reqBodyDataA = strings.NewReader(data)
+	req, err := http.NewRequest("PUT", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users/"+userIdStr, reqBodyDataA)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(apiId, apiSecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
+}
+
+func updateUserWs(userId, workspaceId int) {
+	fmt.Println(workspaceId)
+	workspaceIdStr := strconv.Itoa(workspaceId)
+	apiId, apiSecret := Getapikeys()
+	roleWs, enableWs := updateUserSelectorWs()
+	userIdStr := strconv.Itoa(userId)
+	client := &http.Client{}
+	data := fmt.Sprintf(`{"roles":["%s"],"enabled": %t}`, roleWs, enableWs)
+	var reqBodyDataWs = strings.NewReader(data)
+	req, err := http.NewRequest("PUT", "https://a.blazemeter.com/api/v4/workspaces/"+workspaceIdStr+"/users/"+userIdStr, reqBodyDataWs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(apiId, apiSecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("%s\n", bodyText)
+	var responseBodyUpdateUserWs updateUserResponse
+	var userRoleWs string
+	json.Unmarshal(bodyText, &responseBodyUpdateUserWs)
+	userEmailWs := responseBodyUpdateUserWs.Result.Email
+	userTypeWs := responseBodyUpdateUserWs.Result.Type
+	userStatusWs := responseBodyUpdateUserWs.Result.Enabled
+	fmt.Printf("\n%-25s %-12s %-10s %-10s", "EMAIL", "TYPE", "ENABLE", "ROLE")
+	for i := 0; i < len(responseBodyUpdateUserWs.Result.Roles); i++ {
+		userRoleWs = responseBodyUpdateUserWs.Result.Roles[i]
+	}
+	fmt.Printf("\n%-25s %-12s %-10t %-10s\n\n", userEmailWs, userTypeWs, userStatusWs, userRoleWs)
+}
+func updateUserWsraw(userId, workspaceId int) {
+	fmt.Println(workspaceId)
+	workspaceIdStr := strconv.Itoa(workspaceId)
+	apiId, apiSecret := Getapikeys()
+	roleWs, enableWs := updateUserSelectorWs()
+	userIdStr := strconv.Itoa(userId)
+	client := &http.Client{}
+	data := fmt.Sprintf(`{"roles":["%s"],"enabled": %t}`, roleWs, enableWs)
+	var reqBodyDataWs = strings.NewReader(data)
+	req, err := http.NewRequest("PUT", "https://a.blazemeter.com/api/v4/workspaces/"+workspaceIdStr+"/users/"+userIdStr, reqBodyDataWs)
 	if err != nil {
 		log.Fatal(err)
 	}
