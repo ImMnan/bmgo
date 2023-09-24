2c008ca81abb34c41e09ddb921799e8a977865d6
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-09-24 13:45:36 +0530

    Completed find test subcommand

--- cmd/find/test.go
+++ cmd/find/test.go
@@ -22,22 +22,36 @@ var testCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		testId, _ := cmd.Flags().GetInt("tid")
-		findTest(testId)
+		rawOutput, _ := cmd.Flags().GetBool("raw")
+		if testId != 0 && rawOutput {
+			findTestraw(testId)
+		} else if testId != 0 {
+			findTest(testId)
+		} else {
+			fmt.Println("\nPlease provide a valid Test ID to find the Test")
+			fmt.Println("[bmgo find test --tid <Test id>")
+		}
 	},
 }
 
 func init() {
 	FindCmd.AddCommand(testCmd)
 	testCmd.Flags().Int("tid", 0, "Provide a test Id to find a test")
+	testCmd.MarkFlagRequired("tid")
 }
 
 type FindTestsResponse struct {
 	Result findTestsResult `json:"result"`
 }
 type findTestsResult struct {
-	Name        string `json:"name"`
-	Id          int    `json:"id"`
-	LastRunTime int    `json:"lastRunTime"`
+	Name               string                    `json:"name"`
+	Id                 int                       `json:"id"`
+	LastRunTime        int                       `json:"lastRunTime"`
+	OverrideExecutions []overrideExecutionsArray `json:"overrideExecutions"`
+	ProjectId          int                       `json:"projectId"`
+}
+type overrideExecutionsArray struct {
+	Executor string `json:"executor"`
 }
 
 func findTest(testId int) {
@@ -58,19 +72,47 @@ func findTest(testId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+	//fmt.Printf("%s\n", bodyText)
 	var responseObjectTest FindTestsResponse
 	json.Unmarshal(bodyText, &responseObjectTest)
-	fmt.Printf("%-10s %-20s %-15s\n", "TEST ID", "LAST RUN", "TEST NAME")
 	testName := responseObjectTest.Result.Name
+	fmt.Printf("\nTEST NAME: %s\n", testName)
 	testLastRunEp1 := responseObjectTest.Result.LastRunTime
+	testProjectId := responseObjectTest.Result.ProjectId
 	testLastRunEp := int64(responseObjectTest.Result.LastRunTime)
+	var testExecutor string
+
+	fmt.Println("\n---------------------------------------------------------------------------------------------")
+	fmt.Printf("%-10v %-20s %-10s %-10s\n", "TEST ID", "LAST RUN", "PROJECT", "EXECUTOR")
+	for i := 0; i < len(responseObjectTest.Result.OverrideExecutions); i++ {
+		testExecutor = responseObjectTest.Result.OverrideExecutions[i].Executor
+	}
 	if testLastRunEp1 != 0 {
 		testLastRun := time.Unix(testLastRunEp, 0)
 		testLastRunSp := fmt.Sprint(testLastRun)
-		fmt.Printf("\n%-10v %-20s %-15s", testId, testLastRunSp[0:16], testName)
+		fmt.Printf("%-10v %-20s %-10d %-10s", testId, testLastRunSp[0:16], testProjectId, testExecutor)
 	} else {
 		testLastRun := testLastRunEp1
-		fmt.Printf("\n%-10v %-20v %-15s", testId, testLastRun, testName)
+		fmt.Printf("%-10v %-20v %-10d %-10s\n", testId, testLastRun, testProjectId, testExecutor)
 	}
+}
+func findTestraw(testId int) {
+	testIdStr := strconv.Itoa(testId)
+	apiId, apiSecret := Getapikeys()
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/tests/"+testIdStr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth(apiId, apiSecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
