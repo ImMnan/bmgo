097c55b9b516c56f9ffd63ef99ec6b9ed049e386
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-11-15 08:29:11 +0530

    code enhancement multitestcmd

--- cmd/get/multitest.go
+++ cmd/get/multitest.go
@@ -22,9 +22,9 @@ var multitestsCmd = &cobra.Command{
 	Long: `Use the command to list multi-tests within a specified workspace. The output includes Multi-test NAME, ID, SCENARIOS, etc. The output can be further filtered by specifying a project id by using the --pid flag.
 
 	For example: [bmgo get -w <workspace id> multitests] OR 
-	             [bmgo get -w <workspace id> multitests --pid <project id>]
+	             [bmgo get multitests --pid <project id>]
 	For default: [bmgo get --ws multitests] OR 
-	             [bmgo get --ws multitests --pid <project id>]`,
+	             [bmgo get multitests --pid <project id>]`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ws, _ := cmd.Flags().GetBool("ws")
 		var workspaceId int
@@ -37,14 +37,10 @@ var multitestsCmd = &cobra.Command{
 		projectId, _ := cmd.Flags().GetInt("pid")
 
 		switch {
-		case workspaceId != 0 && projectId != 0 && rawOutput:
-			listMultiTestsWSProjectraw(projectId)
-		case workspaceId != 0 && projectId != 0:
-			listMultiTestsWSProject(projectId)
-		case workspaceId != 0 && rawOutput:
-			listMultiTestsWSraw(workspaceId)
-		case workspaceId != 0:
-			listMultiTestsWS(workspaceId)
+		case (workspaceId != 0 || projectId != 0) && rawOutput:
+			listMultiTestsWSraw(workspaceId, projectId)
+		case (workspaceId != 0 || projectId != 0) && !rawOutput:
+			listMultiTestsWS(workspaceId, projectId)
 		default:
 			cmd.Help()
 		}
@@ -71,13 +67,23 @@ type scenarios struct {
 	TestId int `json:"testId"`
 }
 
-func listMultiTestsWS(workspaceId int) {
+func listMultiTestsWS(workspaceId, projectId int) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
-	workspaceIdStr := strconv.Itoa(workspaceId)
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?workspaceId="+workspaceIdStr+"&limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
+	var req *http.Request
+	var err error
+	if projectId != 0 {
+		projectIdStr := strconv.Itoa(projectId)
+		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?projectId="+projectIdStr+"&limit=0", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		workspaceIdStr := strconv.Itoa(workspaceId)
+		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?workspaceId="+workspaceIdStr+"&limit=0", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(apiId, apiSecret)
@@ -122,85 +128,23 @@ func listMultiTestsWS(workspaceId int) {
 		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
 	}
 }
-func listMultiTestsWSraw(workspaceId int) {
+func listMultiTestsWSraw(workspaceId, projectId int) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
-	workspaceIdStr := strconv.Itoa(workspaceId)
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?workspaceId="+workspaceIdStr+"&limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
-}
-
-func listMultiTestsWSProject(projectId int) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	projectIdStr := strconv.Itoa(projectId)
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?projectId="+projectIdStr+"&limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var responseObjectListMultiTests ListMultiTestsResponse
-	json.Unmarshal(bodyText, &responseObjectListMultiTests)
-	if responseObjectListMultiTests.Error.Code == 0 {
-		fmt.Printf("\n%-10s %-10s %-20s %-10s %-10s\n", "TEST ID", "SCENARIOS", "LAST RUN", "PROJECT", "TEST NAME")
-		for i := 0; i < len(responseObjectListMultiTests.Result); i++ {
-			testName := responseObjectListMultiTests.Result[i].Name
-			testId := responseObjectListMultiTests.Result[i].Id
-			testLastRunEp1 := responseObjectListMultiTests.Result[i].LastRunTime
-			testLastRunEp := int64(responseObjectListMultiTests.Result[i].LastRunTime)
-			testProjectId := responseObjectListMultiTests.Result[i].ProjectId
-			totalscenarios := []int{}
-			for s := 0; s < len(responseObjectListMultiTests.Result[i].TestsForExecutions); s++ {
-				scenario := responseObjectListMultiTests.Result[i].TestsForExecutions[s].TestId
-				totalscenarios = append(totalscenarios, scenario)
-			}
-			// This is because there are epoch values as "0", it converts to a time on 1970s, so we want to condition that here:
-			if testLastRunEp1 != 0 {
-				testLastRun := time.Unix(testLastRunEp, 0)
-				testLastRunSp := fmt.Sprint(testLastRun)
-				fmt.Printf("\n%-10v %-10v %-20s %-10d %-10s", testId, len(totalscenarios), testLastRunSp[0:16], testProjectId, testName)
-			} else {
-				fmt.Printf("\n%-10v %-10v %-20d %-10d %-10s", testId, len(totalscenarios), testLastRunEp1, testProjectId, testName)
-			}
+	var req *http.Request
+	var err error
+	if projectId != 0 {
+		projectIdStr := strconv.Itoa(projectId)
+		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?projectId="+projectIdStr+"&limit=0", nil)
+		if err != nil {
+			log.Fatal(err)
 		}
-		fmt.Println("\n-")
 	} else {
-		errorCode := responseObjectListMultiTests.Error.Code
-		errorMessage := responseObjectListMultiTests.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
-func listMultiTestsWSProjectraw(projectId int) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	projectIdStr := strconv.Itoa(projectId)
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?projectId="+projectIdStr+"&limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
+		workspaceIdStr := strconv.Itoa(workspaceId)
+		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/multi-tests?workspaceId="+workspaceIdStr+"&limit=0", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(apiId, apiSecret)
