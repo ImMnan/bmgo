66627500efc2f54b5ff36d420237d768c86be9af
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-08-10 09:45:16 +0530

    enhanced add opl sub cmd

--- cmd/add/opl.go
+++ cmd/add/opl.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package add
 
@@ -33,10 +33,8 @@ var oplCmd = &cobra.Command{
 		oplName, _ := cmd.Flags().GetString("name")
 		rawOutput, _ := cmd.Flags().GetBool("raw")
 		switch {
-		case accountId != 0 && rawOutput:
-			addOplraw(accountId, oplName)
-		case accountId != 0:
-			addOpl(accountId, oplName)
+		case accountId != 0 && oplName != "":
+			addOpl(accountId, oplName, rawOutput)
 		default:
 			cmd.Help()
 		}
@@ -47,6 +45,8 @@ func init() {
 	AddCmd.AddCommand(oplCmd)
 	oplCmd.Flags().String("name", "", "Name your Private location")
 	oplCmd.MarkFlagRequired("name")
+	oplCmd.Flags().IntP("accountid", "a", 0, " Provide Account ID to add a resource to")
+	oplCmd.Flags().Bool("ac", false, "Use default account Id (bmConfig.yaml)")
 }
 
 type oplResponse struct {
@@ -64,7 +64,7 @@ type oplResult struct {
 	WorkspacesId     []int    `json:"workspacesId"`
 }
 
-func addOpl(accountId int, oplName string) {
+func addOpl(accountId int, oplName string, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	resultWsId := workspaceIdPrompt()
 	resultEPA, resultTPE := oplconfigPrompt()
@@ -88,55 +88,31 @@ func addOpl(accountId int, oplName string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	fmt.Printf("\n%-27s %-15s %-7s %-7s %-10s %-5s\n", "ID", "NAME", "TPE", "EPA", "ACCOUNT", "WORKSPACES")
-	var responseBodyAddOpl oplResponse
-	json.Unmarshal(bodyText, &responseBodyAddOpl)
-	if responseBodyAddOpl.Error.Code == 0 {
-		threadsPerEngine := responseBodyAddOpl.Result.ThreadsPerEngine
-		enginePerAgent := responseBodyAddOpl.Result.Slots
-		oplAccountId := responseBodyAddOpl.Result.AccountId
-		oplWorkspaceId := responseBodyAddOpl.Result.WorkspacesId
-		oplHarbourId := responseBodyAddOpl.Result.Id
-		fmt.Printf("%-27v %-15s %-7v %-7v %-10v %-5v\n", oplHarbourId, oplName, threadsPerEngine, enginePerAgent, oplAccountId, oplWorkspaceId)
-
-		fmt.Println("\n---------------------------------------------------------------------------------------------")
-		fmt.Printf("%-30s\n\n", "FUNCTIONALITIES SUPPORTED")
-		for i := 0; i < len(responseBodyAddOpl.Result.FuncIds); i++ {
-			oplfunctionalities := responseBodyAddOpl.Result.FuncIds[i]
-			fmt.Printf("%-30v\n", oplfunctionalities)
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyAddOpl.Error.Code
-		errorMessage := responseBodyAddOpl.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
+		fmt.Printf("\n%-27s %-15s %-7s %-7s %-10s %-5s\n", "ID", "NAME", "TPE", "EPA", "ACCOUNT", "WORKSPACES")
+		var responseBodyAddOpl oplResponse
+		json.Unmarshal(bodyText, &responseBodyAddOpl)
+		if responseBodyAddOpl.Error.Code == 0 {
+			threadsPerEngine := responseBodyAddOpl.Result.ThreadsPerEngine
+			enginePerAgent := responseBodyAddOpl.Result.Slots
+			oplAccountId := responseBodyAddOpl.Result.AccountId
+			oplWorkspaceId := responseBodyAddOpl.Result.WorkspacesId
+			oplHarbourId := responseBodyAddOpl.Result.Id
+			fmt.Printf("%-27v %-15s %-7v %-7v %-10v %-5v\n", oplHarbourId, oplName, threadsPerEngine, enginePerAgent, oplAccountId, oplWorkspaceId)
 
-func addOplraw(accountId int, oplName string) {
-	apiId, apiSecret := Getapikeys()
-	resultWsId := workspaceIdPrompt()
-	resultEPA, resultTPE := oplconfigPrompt()
-	client := &http.Client{}
-	data := fmt.Sprintf(`{"consoleXms":1024,"consoleXmx":4096,"engineXms":1024,"engineXmx":4096,
-	"name":"%s","slots":%v,"threadsPerEngine":%v,"type":"small","overrideCPU":2,"overrideMemory":4096,
-	"accountId":%v,"workspacesId":[%v]}`, oplName, resultEPA, resultTPE, accountId, resultWsId)
-	reqBodydata := strings.NewReader(data)
-	req, err := http.NewRequest("POST", "https://a.blazemeter.com/api/v4/private-locations", reqBodydata)
-	if err != nil {
-		log.Fatal(err)
+			fmt.Println("\n---------------------------------------------------------------------------------------------")
+			fmt.Printf("%-30s\n\n", "FUNCTIONALITIES SUPPORTED")
+			for i := 0; i < len(responseBodyAddOpl.Result.FuncIds); i++ {
+				oplfunctionalities := responseBodyAddOpl.Result.FuncIds[i]
+				fmt.Printf("%-30v\n", oplfunctionalities)
+			}
+			fmt.Println("\n-")
+		} else {
+			errorCode := responseBodyAddOpl.Error.Code
+			errorMessage := responseBodyAddOpl.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
