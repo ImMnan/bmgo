28711dc8add50fbec45634f0b5f1a5d4f67edd49
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-08-10 09:45:23 +0530

    enhanced add project sub cmd

--- cmd/add/project.go
+++ cmd/add/project.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package add
 
@@ -33,10 +33,8 @@ var projectCmd = &cobra.Command{
 		projectName, _ := cmd.Flags().GetString("name")
 		rawOutput, _ := cmd.Flags().GetBool("raw")
 		switch {
-		case (workspaceId != 0) && (projectName != "") && rawOutput:
-			addProjectraw(projectName, workspaceId)
 		case (workspaceId != 0) && (projectName != ""):
-			addProject(projectName, workspaceId)
+			addProject(projectName, workspaceId, rawOutput)
 		default:
 			cmd.Help()
 		}
@@ -47,6 +45,8 @@ func init() {
 	AddCmd.AddCommand(projectCmd)
 	projectCmd.Flags().String("name", "", "Name your Project")
 	projectCmd.MarkFlagRequired("name")
+	projectCmd.Flags().IntP("workspaceid", "w", 0, " Provide Workspace ID to add a resource to")
+	projectCmd.Flags().Bool("ws", false, "Use default workspace Id (bmConfig.yaml)")
 }
 
 type addProjectResponse struct {
@@ -58,7 +58,7 @@ type addProjectResult struct {
 	Name string `json:"name"`
 }
 
-func addProject(projectName string, workspaceId int) {
+func addProject(projectName string, workspaceId int, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
 	data := fmt.Sprintf(`{
@@ -81,43 +81,21 @@ func addProject(projectName string, workspaceId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	var responseBodyAddProject addProjectResponse
-	json.Unmarshal(bodyText, &responseBodyAddProject)
-	if responseBodyAddProject.Error.Code == 0 {
-		projectIdres := responseBodyAddProject.Result.Id
-		projectNameres := responseBodyAddProject.Result.Name
-		fmt.Printf("\n%-15s %-15s", "PROJECT ID", "NAME")
-		fmt.Printf("\n%-15v %-15s", projectIdres, projectNameres)
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyAddProject.Error.Code
-		errorMessage := responseBodyAddProject.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		var responseBodyAddProject addProjectResponse
+		json.Unmarshal(bodyText, &responseBodyAddProject)
+		if responseBodyAddProject.Error.Code == 0 {
+			projectIdres := responseBodyAddProject.Result.Id
+			projectNameres := responseBodyAddProject.Result.Name
+			fmt.Printf("\n%-15s %-15s", "PROJECT ID", "NAME")
+			fmt.Printf("\n%-15v %-15s", projectIdres, projectNameres)
+			fmt.Println("\n-")
+		} else {
+			errorCode := responseBodyAddProject.Error.Code
+			errorMessage := responseBodyAddProject.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
-}
-func addProjectraw(projectName string, workspaceId int) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	data := fmt.Sprintf(`{
-		"name": "%s", 
-		"description": "Project created through bmgo", 
-		"workspaceId": %v}`, projectName, workspaceId)
-	reqBodyData := strings.NewReader(data)
-	req, err := http.NewRequest("POST", "https://a.blazemeter.com/api/v4/projects", reqBodyData)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
