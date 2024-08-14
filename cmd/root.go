8e2e23aba22cf78c0a8ec81e897b33ff28cc9828
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-08-14 14:28:03 +0530

    update

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,10 @@
 /*
-Copyright © 2023 Manan Patel - github.com/immnan
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/immnan/bmgo/cmd/add"
@@ -16,11 +17,45 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bmgo",
-	Short: "A Blazemeter housekeeping cli",
+	Short: "A Blazemeter CLI tool for Blazemeter Admins",
 	Long:  "A Cli tool used for performing actions within Blazemeter platform",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		version, _ := cmd.Flags().GetBool("version")
+		license, _ := cmd.Flags().GetBool("license")
+		config, _ := cmd.Flags().GetBool("config")
+		if version {
+			fmt.Println("Version: bmgo-0.1.1_beta")
+			fmt.Println("Author:  https://github.com/ImMnan/")
+			fmt.Println(`
+bmgo  Copyright (C) 2024 Manan Patel
+This program comes with ABSOLUTELY NO WARRANTY; 
+This is free software, and you are welcome to redistribute it
+under certain conditions; type "bmgo --license" for details.
+	`)
+		} else if license {
+			fmt.Println(`bmgo - An OpenSource CLI tool for Blazemeter Admins
+Copyright (C) 2024  Manan Patel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`)
+		} else if config {
+			writeConfig()
+		} else {
+			cmd.Help()
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -48,6 +83,9 @@ func init() {
 	// rootCmd.PersistentFlags().StringP("author", "a", "Manan Patel", "author name for copyright attribution")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("version", "v", false, "Version of the installed bmgo!")
+	rootCmd.Flags().BoolP("license", "l", false, "Show license details")
+	rootCmd.Flags().BoolP("config", "c", false, "Add config details")
 	addSubCommand()
 }
