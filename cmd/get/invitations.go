e8470ddd272bfbc950268f2fb666326da31c1a1b
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-04 08:29:14 +0530

    efficient, code clean, functions merge

--- cmd/get/invitations.go
+++ cmd/get/invitations.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
@@ -9,7 +9,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -30,14 +32,7 @@ var invitationsCmd = &cobra.Command{
 			accountId, _ = cmd.Flags().GetInt("accountid")
 		}
 		rawOutput, _ := cmd.Flags().GetBool("raw")
-		if rawOutput {
-			getInvitationsraw(accountId)
-		} else if !rawOutput {
-			getInvitations(accountId)
-		} else {
-			cmd.Help()
-		}
-
+		getInvitations(accountId, rawOutput)
 	},
 }
 
@@ -58,7 +53,7 @@ type invitesResult struct {
 	WorkspacesRoles []string `json:"workspacesRoles"`
 }
 
-func getInvitations(accountId int) {
+func getInvitations(accountId int, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
 	accountIdStr := strconv.Itoa(accountId)
@@ -76,53 +71,40 @@ func getInvitations(accountId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	var responseBodyInvites invitesResponse
-	json.Unmarshal(bodyText, &responseBodyInvites)
-	if responseBodyInvites.Error.Code == 0 {
-		//totalWsRoles := []string{}
-		fmt.Printf("\n%-30s %-20s %-20s %-10s %-5s\n", "INVITEE EMAIL", "ACCOUNT", "WORKSPACE", "AC_ROLE", "WORKSPACE ROLES")
-		for i := 0; i < len(responseBodyInvites.Result); i++ {
-			accountName := responseBodyInvites.Result[i].AccountName
-			userEmail := responseBodyInvites.Result[i].InviteeEmail
-			var workspaceName, acRoles, wsRoles string
-			for w := 0; w < len(responseBodyInvites.Result[i].WorkspaceNames); w++ {
-				workspaceName = responseBodyInvites.Result[i].WorkspaceNames[w]
-			}
-			for ar := 0; ar < len(responseBodyInvites.Result[i].AccountRoles); ar++ {
-				acRoles = responseBodyInvites.Result[i].AccountRoles[ar]
-			}
-			for wr := 0; wr < len(responseBodyInvites.Result[i].WorkspacesRoles); wr++ {
-				wsRoles = responseBodyInvites.Result[i].WorkspacesRoles[wr]
-			}
-
-			fmt.Printf("\n%-30s %-20s %-20s %-10s %-5s", userEmail, accountName, workspaceName, acRoles, wsRoles)
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyInvites.Error.Code
-		errorMessage := responseBodyInvites.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
+		var responseBodyInvites invitesResponse
+		json.Unmarshal(bodyText, &responseBodyInvites)
+		if responseBodyInvites.Error.Code == 0 {
+			//totalWsRoles := []string{}
+			//fmt.Printf("\n%-30s %-20s %-20s %-10s %-5s\n", "INVITEE EMAIL", "ACCOUNT", "WORKSPACE", "AC_ROLE", "WORKSPACE ROLES")
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriter, "INVITEE_EMAIL\tACCOUNT\tWORKSPACE\tAC_ROLE\tWORKSPACE_ROLES")
+			for i := 0; i < len(responseBodyInvites.Result); i++ {
+				accountName := responseBodyInvites.Result[i].AccountName
+				userEmail := responseBodyInvites.Result[i].InviteeEmail
+				var workspaceName, acRoles, wsRoles string
+				for w := 0; w < len(responseBodyInvites.Result[i].WorkspaceNames); w++ {
+					workspaceName = responseBodyInvites.Result[i].WorkspaceNames[w]
+				}
+				for ar := 0; ar < len(responseBodyInvites.Result[i].AccountRoles); ar++ {
+					acRoles = responseBodyInvites.Result[i].AccountRoles[ar]
+				}
+				for wr := 0; wr < len(responseBodyInvites.Result[i].WorkspacesRoles); wr++ {
+					wsRoles = responseBodyInvites.Result[i].WorkspacesRoles[wr]
+				}
 
-func getInvitationsraw(accountId int) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	accountIdStr := strconv.Itoa(accountId)
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/invitations?limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
+				fmt.Printf("\n%-30s %-20s %-20s %-10s %-5s", userEmail, accountName, workspaceName, acRoles, wsRoles)
+				fmt.Fprintf(tabWriter, "%s\t%s\t%s\t%s\t%s\n", userEmail, accountName, workspaceName, acRoles, wsRoles)
+			}
+			tabWriter.Flush()
+			fmt.Println("\n-")
+		} else {
+			errorCode := responseBodyInvites.Error.Code
+			errorMessage := responseBodyInvites.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
