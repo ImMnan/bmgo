6c602c295af554846027ef540de9af5e243bec31
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-05 19:35:53 +0530

    Updated opl subcmd with func efficiency

--- cmd/get/opls.go
+++ cmd/get/opls.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
@@ -9,7 +9,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -39,14 +41,10 @@ var oplsCmd = &cobra.Command{
 			workspaceId, _ = cmd.Flags().GetInt("workspaceid")
 		}
 		rawOutput, _ := cmd.Flags().GetBool("raw")
-		if (workspaceId != 0) && (accountId == 0) && rawOutput {
-			getOplsWSRaw(workspaceId)
-		} else if (accountId != 0) && (workspaceId == 0) && rawOutput {
-			getOplsRaw(accountId)
-		} else if (workspaceId != 0) && (accountId == 0) {
-			getOplsWS(workspaceId)
+		if (workspaceId != 0) && (accountId == 0) {
+			getOplsWS(workspaceId, rawOutput)
 		} else if (accountId != 0) && (workspaceId == 0) {
-			getOpls(accountId)
+			getOpls(accountId, rawOutput)
 		} else {
 			cmd.Help()
 		}
@@ -79,7 +77,7 @@ type ships struct {
 	LastHeartBeat int    `json:"lastHeartBeat"`
 }
 
-func getOpls(accountId int) {
+func getOpls(accountId int, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	accountIdStr := strconv.Itoa(accountId)
 	client := &http.Client{}
@@ -97,63 +95,55 @@ func getOpls(accountId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	fmt.Printf("\n%-25s %-20s %-7s %-7s %-7s %-10s \n", "HARBOUR ID", "NAME", "TPE", "EPA", "AGENTS", "WORKSPACES")
-	var responseBodyAcOpls oplsResponse
-	json.Unmarshal(bodyText, &responseBodyAcOpls)
-	if responseBodyAcOpls.Error.Code == 0 {
-		totalWorkspaces := []int{}
-		for i := 0; i < len(responseBodyAcOpls.Result); i++ {
-			harbourID := responseBodyAcOpls.Result[i].Id
-			oplName := responseBodyAcOpls.Result[i].Name
-			threadsPerEngine := responseBodyAcOpls.Result[i].ThreadsPerEngine
-			enginePerAgent := responseBodyAcOpls.Result[i].Slots
-			for wd := 0; wd < len(responseBodyAcOpls.Result[i].WorkspacesId); wd++ {
-				workspaceList := responseBodyAcOpls.Result[i].WorkspacesId[wd]
-				totalWorkspaces = append(totalWorkspaces, workspaceList)
-			}
-			totalWorkspacesDup := removeDuplicateValuesInt(totalWorkspaces)
-			fmt.Printf("\n%-25s %-20s %-7v %-7v %-7v %-5v", harbourID, oplName, threadsPerEngine, enginePerAgent,
-				len(responseBodyAcOpls.Result[i].ShipsId), totalWorkspacesDup)
-		}
-		fmt.Println("\n\n---------------------------------------------------------------------------------------------")
-		fmt.Printf("%-20s %-20s\n", "NAME", "FUNCTIONALITIES SUPPORTED")
-		for i := 0; i < len(responseBodyAcOpls.Result); i++ {
-			oplName := responseBodyAcOpls.Result[i].Name
-			functAgent := responseBodyAcOpls.Result[i].FuncIds
-			fmt.Printf("\n%-20s %-5s\n", oplName, functAgent)
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyAcOpls.Error.Code
-		errorMessage := responseBodyAcOpls.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		//fmt.Printf("\n%-25s %-7s %-7s %-7s %-20s %-10s\n", "HARBOUR ID", "TPE", "EPA", "AGENTS", "NAME", "WORKSPACES")
+		var responseBodyAcOpls oplsResponse
+		json.Unmarshal(bodyText, &responseBodyAcOpls)
+		if responseBodyAcOpls.Error.Code == 0 {
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriter, "HARBOUR_ID\tTPE\tEPA\tAGENTS\tNAME\tWORKSPACES")
+			for i := 0; i < len(responseBodyAcOpls.Result); i++ {
+				harbourID := responseBodyAcOpls.Result[i].Id
+				oplName := responseBodyAcOpls.Result[i].Name
+				threadsPerEngine := responseBodyAcOpls.Result[i].ThreadsPerEngine
+				enginePerAgent := responseBodyAcOpls.Result[i].Slots
+				totalWorkspaces := []int{}
+				for wd := 0; wd < len(responseBodyAcOpls.Result[i].WorkspacesId); wd++ {
+					workspaceList := responseBodyAcOpls.Result[i].WorkspacesId[wd]
+					totalWorkspaces = append(totalWorkspaces, workspaceList)
+				}
+				totalWorkspacesDup := removeDuplicateValuesInt(totalWorkspaces)
+				//fmt.Printf("\n%-25s %-7v %-7v %-7v %-25s %-5v", harbourID, threadsPerEngine, enginePerAgent, len(responseBodyAcOpls.Result[i].ShipsId), oplName, totalWorkspacesDup)
+				fmt.Fprintf(tabWriter, "%s\t%d\t%d\t%d\t%s\t%d\n", harbourID, threadsPerEngine, enginePerAgent, len(responseBodyAcOpls.Result[i].ShipsId), oplName, totalWorkspacesDup)
+			}
+			tabWriter.Flush()
+			fmt.Println("\n-")
+
+			//fmt.Printf("%-20s %-20s\n", "NAME", "FUNCTIONALITIES SUPPORTED")
+			tabWriterFunc := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriterFunc, "NAME\tFUNCTIONALITIES_SUPPORTED")
+			for i := 0; i < len(responseBodyAcOpls.Result); i++ {
+				oplName := responseBodyAcOpls.Result[i].Name
+				functAgent := responseBodyAcOpls.Result[i].FuncIds
+				//	fmt.Printf("\n%-20s %-5s\n", oplName, functAgent)
+				fmt.Fprintf(tabWriterFunc, "%s\t%s\n", oplName, functAgent)
+			}
+			tabWriterFunc.Flush()
+			fmt.Println("-")
+		} else {
+			errorCode := responseBodyAcOpls.Error.Code
+			errorMessage := responseBodyAcOpls.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
 
 }
-func getOplsRaw(accountId int) {
-	apiId, apiSecret := Getapikeys()
 
-	accountIdStr := strconv.Itoa(accountId)
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/private-locations?limit=0&accountId="+accountIdStr, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
-}
-
-func getOplsWS(workspaceId int) {
+func getOplsWS(workspaceId int, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	workspaceIdStr := strconv.Itoa(workspaceId)
 	client := &http.Client{}
@@ -171,67 +161,61 @@ func getOplsWS(workspaceId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	fmt.Printf("\n%-25s %-25s %-7s %-7s %-7s %-5s \n", "HARBOUR ID", "NAME", "TPE", "EPA", "AGENTS", "WORKSPACES")
-	var responseBodyWsOpls oplsResponse
-	json.Unmarshal(bodyText, &responseBodyWsOpls)
-	if responseBodyWsOpls.Error.Code == 0 {
-		for i := 0; i < len(responseBodyWsOpls.Result); i++ {
-			harbourID := responseBodyWsOpls.Result[i].Id
-			oplName := responseBodyWsOpls.Result[i].Name
-			threadsPerEngine := responseBodyWsOpls.Result[i].ThreadsPerEngine
-			enginePerAgent := responseBodyWsOpls.Result[i].Slots
-			totalWorkspaces := []int{}
-			for wd := 0; wd < len(responseBodyWsOpls.Result[i].WorkspacesId); wd++ {
-				workspaceList := responseBodyWsOpls.Result[i].WorkspacesId[wd]
-				totalWorkspaces = append(totalWorkspaces, workspaceList)
-			}
-			totalWorkspacesDup := removeDuplicateValuesInt(totalWorkspaces)
-			fmt.Printf("\n%-25s %-25s %-7v %-7v %-7v %-5v", harbourID, oplName, threadsPerEngine, enginePerAgent, len(responseBodyWsOpls.Result[i].ShipsId), totalWorkspacesDup)
-		}
-		fmt.Println("\n\n---------------------------------------------------------------------------------------------")
-		fmt.Printf("%-20s %-20s\n", "NAME", "FUNCTIONALITIES SUPPORTED")
-		for i := 0; i < len(responseBodyWsOpls.Result); i++ {
-			oplName := responseBodyWsOpls.Result[i].Name
-			functAgent := responseBodyWsOpls.Result[i].FuncIds
-			fmt.Printf("\n%-20s %-5s\n", oplName, functAgent)
-		}
-		fmt.Println("\n\n---------------------------------------------------------------------------------------------")
-		fmt.Printf("%-30s %-27s %-8s %-10s\n", "NAME", "SHIP ID", "STATE", "SHIP NAME")
-		for i := 0; i < len(responseBodyWsOpls.Result); i++ {
-			oplName := responseBodyWsOpls.Result[i].Name
-			for f := 0; f < len(responseBodyWsOpls.Result[i].Ships); f++ {
-				shipId := responseBodyWsOpls.Result[i].Ships[f].Id
-				shipName := responseBodyWsOpls.Result[i].Ships[f].Name
-				shipStatus := responseBodyWsOpls.Result[i].Ships[f].State
-				fmt.Printf("\n%-30s %-27s %-8s %-10s", oplName, shipId, shipStatus, shipName)
-			}
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyWsOpls.Error.Code
-		errorMessage := responseBodyWsOpls.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
+		//fmt.Printf("\n%-25s %-7s %-7s %-7s %-10s %-20s \n", "HARBOUR ID", "TPE", "EPA", "AGENTS", "WORKSPACES", "NAME")
+		var responseBodyWsOpls oplsResponse
+		json.Unmarshal(bodyText, &responseBodyWsOpls)
+		if responseBodyWsOpls.Error.Code == 0 {
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriter, "HARBOUR_ID\tTPE\tEPA\tAGENTS\tNAME\tWORKSPACES")
+			for i := 0; i < len(responseBodyWsOpls.Result); i++ {
+				harbourID := responseBodyWsOpls.Result[i].Id
+				oplName := responseBodyWsOpls.Result[i].Name
+				threadsPerEngine := responseBodyWsOpls.Result[i].ThreadsPerEngine
+				enginePerAgent := responseBodyWsOpls.Result[i].Slots
+				totalWorkspaces := []int{}
+				for wd := 0; wd < len(responseBodyWsOpls.Result[i].WorkspacesId); wd++ {
+					workspaceList := responseBodyWsOpls.Result[i].WorkspacesId[wd]
+					totalWorkspaces = append(totalWorkspaces, workspaceList)
+				}
+				totalWorkspacesDup := removeDuplicateValuesInt(totalWorkspaces)
+				//	fmt.Printf("\n%-25s %-7v %-7v %-7v %-5v %-20s", harbourID, threadsPerEngine, enginePerAgent,len(responseBodyWsOpls.Result[i].ShipsId), totalWorkspacesDup, oplName)
+				fmt.Fprintf(tabWriter, "%s\t%d\t%d\t%d\t%s\t%d\n", harbourID, threadsPerEngine, enginePerAgent, len(responseBodyWsOpls.Result[i].ShipsId), oplName, totalWorkspacesDup)
+			}
+			tabWriter.Flush()
+			fmt.Println("\n-")
 
-func getOplsWSRaw(workspaceId int) {
-	apiId, apiSecret := Getapikeys()
-	workspaceIdStr := strconv.Itoa(workspaceId)
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/private-locations?workspaceId="+workspaceIdStr+"&limit=0", nil)
-	if err != nil {
-		log.Fatal(err)
+			fmt.Printf("%-20s %-20s\n", "NAME", "FUNCTIONALITIES SUPPORTED")
+			for i := 0; i < len(responseBodyWsOpls.Result); i++ {
+				oplName := responseBodyWsOpls.Result[i].Name
+				functAgent := responseBodyWsOpls.Result[i].FuncIds
+				fmt.Printf("\n%-20s %-5s", oplName, functAgent)
+			}
+			fmt.Println("\n-")
+
+			tabWriterFunc := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriterFunc, "OPL_NAME\tSHIP_ID\tSTATE\tNAME")
+			//	fmt.Printf("%-30s %-27s %-8s %-10s\n", "NAME", "SHIP ID", "STATE", "SHIP NAME")
+			for i := 0; i < len(responseBodyWsOpls.Result); i++ {
+				oplName := responseBodyWsOpls.Result[i].Name
+				for f := 0; f < len(responseBodyWsOpls.Result[i].Ships); f++ {
+					shipId := responseBodyWsOpls.Result[i].Ships[f].Id
+					shipName := responseBodyWsOpls.Result[i].Ships[f].Name
+					shipStatus := responseBodyWsOpls.Result[i].Ships[f].State
+					//	fmt.Printf("\n%-30s %-27s %-8s %-10s", oplName, shipId, shipStatus, shipName)
+					fmt.Fprintf(tabWriterFunc, "%s\t%s\t%s\t%s\n", oplName, shipId, shipStatus, shipName)
+				}
+			}
+			tabWriterFunc.Flush()
+			fmt.Println("-")
+		} else {
+			errorCode := responseBodyWsOpls.Error.Code
+			errorMessage := responseBodyWsOpls.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
