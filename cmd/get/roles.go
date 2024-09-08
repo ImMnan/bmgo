e6e325955bed6e13fa69fef65dd78acdf6d50134
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-08 09:20:20 +0530

    enhanced roles subcmd,

--- cmd/get/roles.go
+++ cmd/get/roles.go
@@ -1,9 +1,10 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,11 +13,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rolesCmd represents the roles command
+// rolesCmd represents the agents command
 var rolesCmd = &cobra.Command{
 	Use:   "roles",
-	Short: "Get a list of Roles in a team",
-	Long:  `.`,
+	Short: "[>]Get roles within an API Monitoring team",
+	Long: `The command returns a list of available roles in an API Monitoring team (default & created), you will need to provide a team id to run the command. Outputs "ROLE NAME", "ROLE ID", etc.
+	
+	For example: [bmgo get -t <team id> roles ] OR 
+	             [bmgo get --team <team id> roles]
+	For default: [bmgo get --tm roles]]`,
 	Run: func(cmd *cobra.Command, args []string) {
 		tm, _ := cmd.Flags().GetBool("tm")
 		var teamId string
@@ -25,8 +30,13 @@ var rolesCmd = &cobra.Command{
 		} else {
 			teamId, _ = cmd.Flags().GetString("teamid")
 		}
-		//rawOutput, _ := cmd.Flags().GetBool("raw")
-		getRolesTm(teamId)
+		rawOutput, _ := cmd.Flags().GetBool("raw")
+		switch {
+		case teamId != "":
+			getRolesTm(teamId, rawOutput)
+		default:
+			cmd.Help()
+		}
 	},
 }
 
@@ -34,14 +44,28 @@ func init() {
 	GetCmd.AddCommand(rolesCmd)
 }
 
-func getRolesTm(teamId string) {
-	bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
+type UserGroupResponse struct {
+	Meta struct {
+		Status string `json:"status"`
+	} `json:"meta"`
+
+	Data []struct {
+		UUID        string   `json:"uuid"`
+		Name        string   `json:"name"`
+		Permissions []string `json:"permissions"`
+	} `json:"data"`
+
+	Error errorResult `json:"error"`
+}
+
+func getRolesTm(teamId string, rawOutput bool) {
+	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://api.runscope.com/teams/"+teamId+"/roles", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Set("Authorization", bearer)
+	req.Header.Set("Authorization", Bearer)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
@@ -51,5 +75,27 @@ func getRolesTm(teamId string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
+	} else {
+		var responseBodyTmRoles UserGroupResponse
+		json.Unmarshal(bodyText, &responseBodyTmRoles)
+		if responseBodyTmRoles.Error.Code == 0 {
+			for _, group := range responseBodyTmRoles.Data {
+				UUID := group.UUID
+				name := group.Name
+				//var permissions []string
+				fmt.Printf("UUID: %s\nROLENAME: %s\nPERMISSIONS: ", UUID, name)
+				for i := 0; i < len(group.Permissions); i++ {
+					fmt.Println(group.Permissions[i])
+				}
+				fmt.Println("\n-")
+			}
+		} else {
+			errorCode := responseBodyTmRoles.Error.Status
+			errorMessage := responseBodyTmRoles.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
+
+	}
 }
