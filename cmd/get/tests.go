e3ceb478f8f0c89fdfd1c4996d23a30fdd43370c
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-09 20:00:16 +0530

    Better output efficient func

--- cmd/get/tests.go
+++ cmd/get/tests.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
@@ -9,7 +9,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"sync"
+	"text/tabwriter"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -33,14 +36,21 @@ var testsCmd = &cobra.Command{
 		} else {
 			workspaceId, _ = cmd.Flags().GetInt("workspaceid")
 		}
+		tm, _ := cmd.Flags().GetBool("tm")
+		var teamId string
+		if tm {
+			teamId = defaultTeam()
+		} else {
+			teamId, _ = cmd.Flags().GetString("teamid")
+		}
 		rawOutput, _ := cmd.Flags().GetBool("raw")
 		projectId, _ := cmd.Flags().GetInt("pid")
 
 		switch {
-		case !rawOutput && (workspaceId != 0 || projectId != 0):
-			listTestsWS(workspaceId, projectId)
-		case rawOutput && (workspaceId != 0 || projectId != 0):
-			listTestsWSraw(workspaceId, projectId)
+		case (workspaceId != 0 || projectId != 0):
+			listTestsWS(workspaceId, projectId, rawOutput)
+		case (workspaceId == 0 && projectId == 0) && teamId != "":
+			getTeamBuckets(teamId, rawOutput)
 		default:
 			cmd.Help()
 		}
@@ -70,10 +80,10 @@ type config struct {
 	EnableLoadConfiguration bool `json:"enableLoadConfiguration"`
 }
 type executiondetails struct {
-	Locations string
+	Locations []string `json:"locations"`
 }
 
-func listTestsWS(workspaceId, projectId int) {
+func listTestsWS(workspaceId, projectId int, rawOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
 	var req *http.Request
@@ -102,52 +112,50 @@ func listTestsWS(workspaceId, projectId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var responseObjectListTests ListTestsResponse
-	json.Unmarshal(bodyText, &responseObjectListTests)
-	if responseObjectListTests.Error.Code == 0 {
-		fmt.Printf("\n%-10s %-20s %-10s %-15s\n", "TEST ID", "LAST RUN", "PROJECT", "TEST NAME")
-		for i := 0; i < len(responseObjectListTests.Result); i++ {
-			testName := responseObjectListTests.Result[i].Name
-			testId := responseObjectListTests.Result[i].Id
-			testLastRunEp1 := responseObjectListTests.Result[i].LastRunTime
-			testProjectId := responseObjectListTests.Result[i].ProjectId
-			testLastRunEp := int64(responseObjectListTests.Result[i].LastRunTime)
-			// This is because there are epoch values as "0", it converts to a time on 1970s, so we want to condition that here:
-			if testLastRunEp1 != 0 {
-				testLastRun := time.Unix(testLastRunEp, 0)
-				testLastRunSp := fmt.Sprint(testLastRun)
-				fmt.Printf("\n%-10v %-20s %-10d %-15s", testId, testLastRunSp[0:16], testProjectId, testName)
-			} else {
-				fmt.Printf("\n%-10v %-20v %-10d %-15s", testId, testLastRunEp1, testProjectId, testName)
-			}
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseObjectListTests.Error.Code
-		errorMessage := responseObjectListTests.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		var responseObjectListTests ListTestsResponse
+		json.Unmarshal(bodyText, &responseObjectListTests)
+		if responseObjectListTests.Error.Code == 0 {
+			// Create tabwriter
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			fmt.Fprintln(tabWriter, "TEST ID\tLAST RUN\tPROJECT\tTEST NAME")
+
+			for i := 0; i < len(responseObjectListTests.Result); i++ {
+				testName := responseObjectListTests.Result[i].Name
+				testId := responseObjectListTests.Result[i].Id
+				testLastRunEp1 := responseObjectListTests.Result[i].LastRunTime
+				testProjectId := responseObjectListTests.Result[i].ProjectId
+				testLastRunEp := int64(responseObjectListTests.Result[i].LastRunTime)
+				// This is because there are epoch values as "0", it converts to a time on 1970s, so we want to condition that here:
+				if testLastRunEp1 != 0 {
+					testLastRun := time.Unix(testLastRunEp, 0)
+					testLastRunSp := fmt.Sprint(testLastRun)
+					fmt.Fprintf(tabWriter, "%d\t%s\t%d\t%s\n", testId, testLastRunSp[0:16], testProjectId, testName)
+				} else {
+					fmt.Fprintf(tabWriter, "%d\t%d\t%d\t%s\n", testId, testLastRunEp1, testProjectId, testName)
+				}
+			}
+			tabWriter.Flush()
+			fmt.Println("\n-")
+		} else {
+			errorCode := responseObjectListTests.Error.Code
+			errorMessage := responseObjectListTests.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
 }
-func listTestsWSraw(workspaceId, projectId int) {
-	apiId, apiSecret := Getapikeys()
+
+func getTeamBuckets(teamId string, rawOutput bool) {
+	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
 	client := &http.Client{}
-	var req *http.Request
-	var err error
-	if projectId != 0 {
-		projectIdStr := strconv.Itoa(projectId)
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/tests?projectId="+projectIdStr+"&limit=0", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		workspaceIdStr := strconv.Itoa(workspaceId)
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/tests?workspaceId="+workspaceIdStr+"&limit=0", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+	req, err := http.NewRequest("GET", "https://api.runscope.com/teams/"+teamId, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(apiId, apiSecret)
+	req.Header.Set("Authorization", Bearer)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
@@ -157,5 +165,130 @@ func listTestsWSraw(workspaceId, projectId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", bodyText)
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
+	} else {
+		var responseBodyTeamInfo teamInfo
+		json.Unmarshal(bodyText, &responseBodyTeamInfo)
+		var wg sync.WaitGroup
+		results := make(chan StepDetail)
+
+		for _, bucket := range responseBodyTeamInfo.Buckets {
+			wg.Add(1)
+			go func(bucketKey string) {
+				defer wg.Done()
+				listTestsTm(bucketKey, results)
+			}(bucket.Key)
+		}
+
+		// Close the channel when all goroutines are done
+		go func() {
+			wg.Wait()
+			close(results)
+		}()
+
+		// Print results in a table
+		printResultsTable(results)
+	}
+}
+
+type result struct {
+	Data []bucketTests `json:"data"`
+}
+type bucketTests struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type testResult struct {
+	Data dataTests `json:"data"`
+}
+type dataTests struct {
+	Steps []steps `json:"steps"`
+}
+type steps struct {
+	Id  string `json:"id"`
+	Url string `json:"url"`
+}
+
+// This is for channel
+type StepDetail struct {
+	TestID string
+	URL    string
+}
+
+func listTestsTm(bucketKey string, results chan<- StepDetail) {
+	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.runscope.com/buckets/"+bucketKey+"/tests", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	req.Header.Set("Authorization", Bearer)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var response result
+	json.Unmarshal(bodyText, &response)
+	var wg sync.WaitGroup
+
+	for _, test := range response.Data {
+		wg.Add(1)
+		go func(testId string) {
+			defer wg.Done()
+			findTestTm(bucketKey, testId, results)
+		}(test.Id)
+	}
+
+	wg.Wait()
+}
+
+func findTestTm(bucketKey, testId string, results chan<- StepDetail) {
+	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.runscope.com/buckets/"+bucketKey+"/tests/"+testId, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	req.Header.Set("Authorization", Bearer)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var response testResult
+	json.Unmarshal(bodyText, &response)
+	for _, step := range response.Data.Steps {
+		results <- StepDetail{
+			TestID: testId,
+			URL:    step.Url,
+		}
+	}
+}
+
+func printResultsTable(results <-chan StepDetail) {
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(writer, "Test ID\tStep URL")
+	//fmt.Fprintln(writer, "-------\t--------")
+
+	for result := range results {
+		fmt.Fprintf(writer, "%s\t%s\n", result.TestID, result.URL)
+	}
+	writer.Flush()
 }
