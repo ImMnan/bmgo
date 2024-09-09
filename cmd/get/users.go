291d9ec14c824208bb7c47e9bfd6d7b2770f2e30
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-09 20:02:54 +0530

    update
    Major changes, efficiency, better output with additional optional outputs

--- cmd/get/users.go
+++ cmd/get/users.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
@@ -9,7 +9,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"text/tabwriter"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -17,7 +20,7 @@ import (
 // usersCmd represents the users command
 var usersCmd = &cobra.Command{
 	Use:   "users",
-	Short: "Get a list of users part of the account",
+	Short: "[>]Get a list of users part of the account",
 	Long: `Use the command to list Users within a specified account, team (API monitoring), or a workspace. The output includes User ID, Name, Roles, Email, etc. The output can be further filtered by switching disabled flag as true to only display disabled users, --disabled.
 	
 	For example: [bmgo get -w <workspace id> users] OR
@@ -54,20 +57,14 @@ var usersCmd = &cobra.Command{
 		}
 		rawOutput, _ := cmd.Flags().GetBool("raw")
 		disabledUsers, _ := cmd.Flags().GetBool("disabled")
-
+		csvOutput, _ := cmd.Flags().GetBool("csv")
 		switch {
-		case accountId != 0 && workspaceId == 0 && !rawOutput && teamId == "":
-			getUsersA(accountId, disabledUsers)
-		case accountId != 0 && workspaceId == 0 && rawOutput && teamId == "":
-			getUsersAraw(accountId, disabledUsers)
-		case accountId == 0 && workspaceId != 0 && !rawOutput && teamId == "":
-			getUsersWS(workspaceId, disabledUsers)
-		case accountId == 0 && workspaceId != 0 && rawOutput && teamId == "":
-			getUsersWSraw(workspaceId, disabledUsers)
-		case accountId == 0 && workspaceId == 0 && !rawOutput && teamId != "":
-			getUsersTm(teamId)
-		case accountId == 0 && workspaceId == 0 && rawOutput && teamId != "":
-			getUsersTmraw(teamId)
+		case accountId != 0 && workspaceId == 0 && teamId == "":
+			getUsersA(accountId, disabledUsers, rawOutput, csvOutput)
+		case accountId == 0 && workspaceId != 0 && teamId == "":
+			getUsersWS(workspaceId, disabledUsers, rawOutput, csvOutput)
+		case accountId == 0 && workspaceId == 0 && teamId != "":
+			getUsersTm(teamId, rawOutput, csvOutput)
 		default:
 			cmd.Help()
 		}
@@ -77,6 +74,7 @@ var usersCmd = &cobra.Command{
 func init() {
 	GetCmd.AddCommand(usersCmd)
 	usersCmd.Flags().Bool("disabled", false, "[Optional] will show disabled users only")
+	usersCmd.Flags().Bool("csv", false, "This will output in csv format")
 }
 
 type usersResponse struct {
@@ -91,16 +89,18 @@ type usersResult struct {
 	DisplayName string   `json:"displayName"`
 	Enabled     bool     `json:"enabled"`
 	Roles       []string `json:"roles"`
+	LastAccess  int      `json:"lastAccess"`
 }
 type usersData struct {
-	Uuid       string `json:"uuid"`
-	Email      string `json:"email"`
-	Role_name  string `json:"role_name"`
-	Created_at string `json:"created_at"`
-	Name       string `json:"name"`
+	Uuid          string `json:"uuid"`
+	Email         string `json:"email"`
+	Role_name     string `json:"role_name"`
+	Created_at    string `json:"created_at"`
+	Name          string `json:"name"`
+	Last_login_at string `json:"last_login_at"`
 }
 
-func getUsersA(accountId int, disabledUsers bool) {
+func getUsersA(accountId int, disabledUsers, rawOutput, csvOutput bool) {
 	apiId, apiSecret := Getapikeys()
 
 	client := &http.Client{}
@@ -108,7 +108,7 @@ func getUsersA(accountId int, disabledUsers bool) {
 	var req *http.Request
 	var err error
 	if disabledUsers {
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users?limit=1000&enabled=false", nil)
+		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users?limit=-1&enabled=false", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -128,62 +128,59 @@ func getUsersA(accountId int, disabledUsers bool) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Printf("%s\n", bodyText)
-	var responseBodyAUsers usersResponse
-	json.Unmarshal(bodyText, &responseBodyAUsers)
-	if responseBodyAUsers.Error.Code == 0 {
-		fmt.Printf("\n%-10s %-25s %-8s %-35s %-5s\n", "ID", "DISPLAY NAME", "ENABLED", "EMAIL", "ROLES")
-		//	rolesListTotal := []string{}
-		for i := 0; i < len(responseBodyAUsers.Result); i++ {
-			userIdWS := responseBodyAUsers.Result[i].Id
-			displayNameWS := responseBodyAUsers.Result[i].DisplayName
-			emailIdWS := responseBodyAUsers.Result[i].Email
-			enabledUserWS := responseBodyAUsers.Result[i].Enabled
-			var totalRoles []string
-			for r := 0; r < len(responseBodyAUsers.Result[i].Roles); r++ {
-				rolesArr := responseBodyAUsers.Result[i].Roles[r]
-				totalRoles = append(totalRoles, rolesArr)
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
+	} else {
+		var responseBodyAUsers usersResponse
+		json.Unmarshal(bodyText, &responseBodyAUsers)
+		if responseBodyAUsers.Error.Code == 0 {
+			//	fmt.Printf("\n%-10s %-25s %-8s %-5s        %-35s\n", "ID", "DISPLAY NAME", "ENABLED", "ROLES", "EMAIL")
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			if csvOutput {
+				fmt.Fprintln(tabWriter, "ID,DISPLAY_NAME,ENABLED,ROLES,EMAIL,LAST_ACCESS")
+			} else {
+				fmt.Fprintln(tabWriter, "ID\tDISPLAY_NAME\tENABLED\tROLES\tEMAIL\tLAST_ACCESS")
 			}
-			fmt.Printf("\n%-10v %-25s %-8t %-35s %-5s", userIdWS, displayNameWS, enabledUserWS, emailIdWS, totalRoles)
+			//fmt.Fprintln(tabWriter, "ID\tDISPLAY_NAME\tENABLED\tROLES\tEMAIL\tLAST_ACCESS")
+			//fmt.Printf("\n%-28s %-8s %-18s %-10s\n", "SHIP ID", "STATE", "LA
+			//	rolesListTotal := []string{}
+			for i := 0; i < len(responseBodyAUsers.Result); i++ {
+				userIdWS := responseBodyAUsers.Result[i].Id
+				displayNameWS := responseBodyAUsers.Result[i].DisplayName
+				emailIdWS := responseBodyAUsers.Result[i].Email
+				enabledUserWS := responseBodyAUsers.Result[i].Enabled
+				var totalRoles []string
+				for r := 0; r < len(responseBodyAUsers.Result[i].Roles); r++ {
+					rolesArr := responseBodyAUsers.Result[i].Roles[r]
+					totalRoles = append(totalRoles, rolesArr)
+				}
+				var lastAccessStr string
+				lastAccessEp := int64(responseBodyAUsers.Result[i].LastAccess)
+				// This is because there are epoch values as "0", it converts to a time on 1970s, so we want to condition that here:
+				if lastAccessEp != 0 {
+					lastAccess := time.Unix(lastAccessEp, 0)
+					lastAccessStr = fmt.Sprint(lastAccess)
+				} else {
+					lastAccessStr = "NA"
+				}
+				if csvOutput {
+					fmt.Printf("%d,%s,%t,%s,%s,%s\n", userIdWS, displayNameWS, enabledUserWS, totalRoles, emailIdWS, lastAccessStr)
+				} else {
+					fmt.Fprintf(tabWriter, "%d\t%s\t%t\t%s\t%s\t%s\n", userIdWS, displayNameWS, enabledUserWS, totalRoles, emailIdWS, lastAccessStr)
+				}
+			}
+			tabWriter.Flush()
+			fmt.Println("-")
+		} else {
+			errorCode := responseBodyAUsers.Error.Code
+			errorMessage := responseBodyAUsers.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
 		}
-		fmt.Println("\n-")
-	} else {
-		errorCode := responseBodyAUsers.Error.Code
-		errorMessage := responseBodyAUsers.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
 	}
-}
-func getUsersAraw(accountId int, disabledUsers bool) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	accountIdStr := strconv.Itoa(accountId)
-	var req *http.Request
-	var err error
-	if disabledUsers {
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users?limit=1000&enabled=false", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/users?limit=-1&enabled=true", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
 
-func getUsersWS(workspaceId int, disabledUsers bool) {
+func getUsersWS(workspaceId int, disabledUsers, rawOutput, csvOutput bool) {
 	apiId, apiSecret := Getapikeys()
 	client := &http.Client{}
 	workspaceIdStr := strconv.Itoa(workspaceId)
@@ -210,56 +207,53 @@ func getUsersWS(workspaceId int, disabledUsers bool) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var responseBodyWsUsers usersResponse
-	json.Unmarshal(bodyText, &responseBodyWsUsers)
-	if responseBodyWsUsers.Error.Code == 0 {
-		fmt.Printf("\n%-10s %-25s %-12s %-10s %-10s\n", "ID", "DISPLAY NAME", "ROLES", "ENABLED", "EMAIL")
-		//	rolesListTotal := []string{}
-		for i := 0; i < len(responseBodyWsUsers.Result); i++ {
-			userIdWS := responseBodyWsUsers.Result[i].Id
-			displayNameWS := responseBodyWsUsers.Result[i].DisplayName
-			emailIdWS := responseBodyWsUsers.Result[i].Email
-			enabledUserWS := responseBodyWsUsers.Result[i].Enabled
-			fmt.Printf("\n%-10v %-25s %-12s %-10t %-10s", userIdWS, displayNameWS, responseBodyWsUsers.Result[i].Roles[0], enabledUserWS, emailIdWS)
-		}
-		fmt.Println("\n-")
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
 	} else {
-		errorCode := responseBodyWsUsers.Error.Code
-		errorMessage := responseBodyWsUsers.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
-func getUsersWSraw(workspaceId int, disabledUsers bool) {
-	apiId, apiSecret := Getapikeys()
-	client := &http.Client{}
-	workspaceIdStr := strconv.Itoa(workspaceId)
-	var req *http.Request
-	var err error
-	if disabledUsers {
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/workspaces/"+workspaceIdStr+"/users?limit=-500&enabled=false", nil)
-		if err != nil {
-			log.Fatal(err)
+		var responseBodyWsUsers usersResponse
+		json.Unmarshal(bodyText, &responseBodyWsUsers)
+		if responseBodyWsUsers.Error.Code == 0 {
+			//	fmt.Printf("\n%-10s %-25s %-12s %-10s %-10s\n", "ID", "DISPLAY NAME", "ROLES", "ENABLED", "EMAIL")
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			if csvOutput {
+				fmt.Fprintln(tabWriter, "ID,DISPLAY_NAME,ROLES,ENABLED,EMAIL,LAST_ACCESS")
+			} else {
+				fmt.Fprintln(tabWriter, "ID\tDISPLAY_NAME\tROLES\tENABLED\tEMAIL\tLAST_ACCESS")
+			}
+			//fmt.Fprintln(tabWriter, "ID\tDISPLAY_NAME\tROLES\tENABLED\tEMAIL\tLAST_ACCESS")
+			for i := 0; i < len(responseBodyWsUsers.Result); i++ {
+				userIdWS := responseBodyWsUsers.Result[i].Id
+				displayNameWS := responseBodyWsUsers.Result[i].DisplayName
+				emailIdWS := responseBodyWsUsers.Result[i].Email
+				enabledUserWS := responseBodyWsUsers.Result[i].Enabled
+				var lastAccessStr string
+				lastAccessEp := int64(responseBodyWsUsers.Result[i].LastAccess)
+				// This is because there are epoch values as "0", it converts to a time on 1970s, so we want to condition that here:
+				if lastAccessEp != 0 {
+					lastAccess := time.Unix(lastAccessEp, 0)
+					lastAccessStr = fmt.Sprint(lastAccess)
+				} else {
+					lastAccessStr = "NA"
+				}
+				if csvOutput {
+					fmt.Printf("%d,%s,%s,%t,%s,%s\n", userIdWS, displayNameWS, responseBodyWsUsers.Result[i].Roles[0], enabledUserWS, emailIdWS, lastAccessStr)
+				} else {
+					fmt.Fprintf(tabWriter, "%d\t%s\t%s\t%t\t%s\t%s\n", userIdWS, displayNameWS, responseBodyWsUsers.Result[i].Roles[0], enabledUserWS, emailIdWS, lastAccessStr)
+				}
+				//fmt.Printf("\n%-10v %-25s %-12s %-10t %-10s", userIdWS, displayNameWS, responseBodyWsUsers.Result[i].Roles[0], enabledUserWS, emailIdWS)
+			}
+			tabWriter.Flush()
+			fmt.Println("-")
+		} else {
+			errorCode := responseBodyWsUsers.Error.Code
+			errorMessage := responseBodyWsUsers.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
 		}
-	} else {
-		req, err = http.NewRequest("GET", "https://a.blazemeter.com/api/v4/workspaces/"+workspaceIdStr+"/users?limit=-1&enabled=true", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
-	req.SetBasicAuth(apiId, apiSecret)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
 
-func getUsersTm(teamId string) {
+func getUsersTm(teamId string, rawOutput, csvOutput bool) {
 	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://api.runscope.com/teams/"+teamId+"/people", nil)
@@ -275,43 +269,42 @@ func getUsersTm(teamId string) {
 	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if rawOutput {
+		fmt.Printf("%s\n", bodyText)
+	} else {
+		var responseBodyTmUsers usersResponse
+		json.Unmarshal(bodyText, &responseBodyTmUsers)
+		if responseBodyTmUsers.Error.Status == 0 {
+			//	fmt.Printf("\n%-38s %-14s %-28s %-10s\n", "UUID", "ROLES", "NAME", "EMAIL")
+			tabWriter := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+			// Print headers
+			if csvOutput {
+				fmt.Fprintln(tabWriter, "UUID,ROLES,NAME,EMAIL,LAST_ACCESS")
+			} else {
+				fmt.Fprintln(tabWriter, "UUID\tROLES\tNAME\tEMAIL\tLAST_ACCESS")
+			}
+			for i := 0; i < len(responseBodyTmUsers.Data); i++ {
+				userIdTm := responseBodyTmUsers.Data[i].Uuid
+				userNameTm := responseBodyTmUsers.Data[i].Name
+				userEmailTm := responseBodyTmUsers.Data[i].Email
+				userRoleTm := responseBodyTmUsers.Data[i].Role_name
+				lastAccessedTm := responseBodyTmUsers.Data[i].Last_login_at
+				//	userCreatedTm := responseBodyTmUsers.Data[i].Created_at
+				//fmt.Printf("\n%-38s %-14s %-28s %-10s", userIdTm, userRoleTm, userNameTm, userEmailTm)
+				if csvOutput {
+					fmt.Printf("%s,%s,%s,%s,%s\n", userIdTm, userRoleTm, userNameTm, userEmailTm, lastAccessedTm[5:16])
+				} else {
+					fmt.Fprintf(tabWriter, "%s\t%s\t%s\t%s\t%s\n", userIdTm, userRoleTm, userNameTm, userEmailTm, lastAccessedTm[5:16])
+				}
+			}
+			tabWriter.Flush()
+			fmt.Println("-")
+		} else {
+			errorCode := responseBodyTmUsers.Error.Status
+			errorMessage := responseBodyTmUsers.Error.Message
+			fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
+		}
 	}
 	//fmt.Printf("%s\n", bodyText)
-	var responseBodyTmUsers usersResponse
-	json.Unmarshal(bodyText, &responseBodyTmUsers)
-	if responseBodyTmUsers.Error.Status == 0 {
-		fmt.Printf("\n%-38s %-14s %-28s %-10s\n", "UUID", "ROLES", "NAME", "EMAIL")
-		for i := 0; i < len(responseBodyTmUsers.Data); i++ {
-			userIdTm := responseBodyTmUsers.Data[i].Uuid
-			userNameTm := responseBodyTmUsers.Data[i].Name
-			userEmailTm := responseBodyTmUsers.Data[i].Email
-			userRoleTm := responseBodyTmUsers.Data[i].Role_name
-			//	userCreatedTm := responseBodyTmUsers.Data[i].Created_at
-			fmt.Printf("\n%-38s %-14s %-28s %-10s", userIdTm, userRoleTm, userNameTm, userEmailTm)
-		}
-		fmt.Println("\n-")
-	} else {
-		errorCode := responseBodyTmUsers.Error.Status
-		errorMessage := responseBodyTmUsers.Error.Message
-		fmt.Printf("\nError code: %v\nError Message: %v\n\n", errorCode, errorMessage)
-	}
-}
-func getUsersTmraw(teamId string) {
-	Bearer := fmt.Sprintf("Bearer %v", GetPersonalAccessToken())
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.runscope.com/teams/"+teamId+"/people", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Authorization", Bearer)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	bodyText, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", bodyText)
 }
