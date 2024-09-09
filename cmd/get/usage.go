869ea0d0ee5eb392bf452f2db0d4f131bc6be573
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-09-09 20:00:51 +0530

    Better output efficient funct for usage report

--- cmd/get/usage.go
+++ cmd/get/usage.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Manan Patel - github.com/immnan
 */
 package get
 
@@ -20,10 +20,10 @@ var usageCmd = &cobra.Command{
 	Short: "Get Usage report for an account",
 	Long: `Use the command to get CSV based output of the account usage report by specifying the account Id. BlazeMeter Usage Reports provide metrics about the utilization of BlazeMeter in your organization. These metrics include high-level utilization data that is available either in graphs or JSON payloads, aggregated daily. You can also download detailed (per-test) utilization data in CSV format using this command. You will need to specify the From data and To data of this report & bmgo will output the report for specified period in CSV. The output can be copied to a csv file by using append function as the output is CSV, it is dificult to read through the terminal.  
 
-	For example: [bmgo get -a <account id> usage --from YYYY/MM/DD --to YYYY/MM/DD] OR
-                 [bmgo get -a <account id> usage --from YYYY/MM/DD --to YYYY/MM/DD > usage.csv]
-	For default: [bmgo get --ac usage --from YYYY/MM/DD --to YYYY/MM/DD] OR
-	             [bmgo get --ac usage --from YYYY/MM/DD --to YYYY/MM/DD > usage.csv]`,
+	For example: [bmgo get -a <account id> usage --from YYYY-MM-DD --to YYYY-MM-DD] OR
+                 [bmgo get -a <account id> usage --from YYYY-MM-DD --to YYYY-MM-DD > usage.csv]
+	For default: [bmgo get --ac usage --from YYYY-MM-DD --to YYYY-MM-DD] OR
+	             [bmgo get --ac usage --from YYYY-MM-DD --to YYYY-MM-DD > usage.csv]`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ac, _ := cmd.Flags().GetBool("ac")
 		var accountId int
@@ -71,7 +71,7 @@ func getUsage(accountId int, fromDate, toDate string) {
 	}
 	toEpoch := toTime.Unix()
 	toEpochStr := fmt.Sprint(toEpoch)
-
+	//fmt.Println(fromEpoch, toEpochStr)
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://a.blazemeter.com/api/v4/accounts/"+accountIdStr+"/reports/usage/tests/credits?daysInterval=1&toDate="+toEpochStr+"&fromDate="+fromEpochStr+"&download=true", nil)
 	if err != nil {
